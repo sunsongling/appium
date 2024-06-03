c9baee9932cb464c697634f645f3c97295b38abf
sunsongling <137742294+sunsongling@users.noreply.github.com>
2024-06-03 19:02:53 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ module.exports = {
     userName : '101282963125-cRbcQvb2',                            //代理ip 用户名
     password : 'd2b8d4664c0d08ce980b046cff8007a7',                 //代理ip 密码
     proxyUrl : 'gate1.ipweb.cc',                                   //代理ip 服务器地址
-    //url      :'https://gamezenithalx.top/' ,                       //跑广告网站地址
-    url      :'https://topgameconsult.top/' ,                       //跑广告网站地址
-    frequency: 1,                                                //点击率
+    //url      :'www.google.com' ,                       //跑广告网站地址
+    url      :'https://www.shutugames.com' ,                       //跑广告网站地址
+    frequency: 100,                                                //点击率
     openChild: 30,                                                  //打开二级页面概率
     device_id: 'VM010010008056',                                   //云机ID
     availability_zone: 5                                           //云机所在区
